d7bdbcaf79541b7302f8fa836e46988810918eda
HaiderMuhammad <haidermuhammadk99@gmail.com>
2023-04-12 02:18:45 +0300

    auth service

--- lib/app/global_widgets/custom_text_field.dart
+++ lib/app/global_widgets/custom_text_field.dart
@@ -15,36 +15,24 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(isFullName ?? false) {
-      return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-          margin: const EdgeInsets.only(right: 10, left: 10, top: 25),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color.fromRGBO(196,137,198,.1),
-                  blurRadius: 10,
-                  offset: Offset(0,.1),
-                )
-              ]
-          ),
-          child: TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: onChanged,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Full Name',
-                hintStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
-              ),
-              validator: validator
-          )
-
+      return textFieldWidget(
+        hintText: 'Full Name'
       );
     }
     if(isEmail ?? false) {
-      return Container(
+      return textFieldWidget(
+          hintText: 'Email'
+      );
+    }
+    else{
+      return textFieldWidget(
+          hintText: 'Password'
+      );
+    }
+  }
+
+  Widget textFieldWidget({String? hintText}){
+    return Container(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
         margin: const EdgeInsets.only(right: 10, left: 10, top: 25),
         decoration: BoxDecoration(
@@ -59,76 +47,21 @@ class CustomTextField extends StatelessWidget {
             ]
         ),
         child: TextFormField(
-          keyboardType: TextInputType.emailAddress,
-          onChanged: onChanged,
-          textInputAction: TextInputAction.next,
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Email',
-            hintStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
-          ),
-          validator: validator
+            keyboardType: TextInputType.emailAddress,
+            onChanged: onChanged,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: hintText,
+              hintStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+            ),
+            validator: validator
         )
 
     );
-    }
-    else if(isPassword ?? false) {
-      return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-          margin: const EdgeInsets.only(right: 10, left: 10, top: 25),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color.fromRGBO(196,137,198,.1),
-                  blurRadius: 10,
-                  offset: Offset(0,.1),
-                )
-              ]
-          ),
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: onChanged,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Password',
-              hintStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
-            ),
-            validator: validator
-          )
-
-      );
-    }
-    else{
-      return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-          margin: const EdgeInsets.only(right: 10, left: 10, top: 25),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color.fromRGBO(196,137,198,.1),
-                  blurRadius: 10,
-                  offset: Offset(0,.1),
-                )
-              ]
-          ),
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: onChanged,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              hintText: 'hint',
-              hintStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
-            ),
-            validator: validator
-          )
-
-      );
-    }
   }
 }
+
+
+
+
